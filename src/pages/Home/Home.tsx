20e76a1c7099b146d632541adac3a804f207a5ff
Satira <arrr_r@vk.com>
2022-10-02 23:41:50 +0300

    initial project

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import cl from './Home.module.scss';
+
+interface Props {
+
+}
+
+const Home = (props: Props) => {
+    return (
+        <div className={cl.home}>
+            Home
+        </div>
+    );
+};
+
+export default Home;
