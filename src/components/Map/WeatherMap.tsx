907e9d8459ac4d4030080b37c81be5d5a0d2285d
antisedativ <antisedativ@mail.ru>
2022-12-17 02:45:50 +0300

    fix| added formik | added yup

--- src/components/Map/WeatherMap.tsx
+++ src/components/Map/WeatherMap.tsx
@@ -7,37 +7,37 @@ import Map from "./Map";
 import {mapKey} from "../../services/keys";
 
 export const WeatherMap:React.FC = () => {
-    // const {location, current} = useCustomSelector(state => state.currentWeatherSliceReducer.weather)
-    //
-    // useEffect(() => {
-    //     let map: any;
-    //     load().then((mapglAPI) => {
-    //         map = new mapglAPI.Map('map-container', {
-    //             center: [location.lon, location.lat],
-    //             zoom: 12,
-    //             key: `${mapKey}`,
-    //             style: 'efac10b3-c66c-40de-8165-decca561250c'
-    //         });
-    //
-    //         const marker = new mapglAPI.Marker(map, {
-    //             coordinates: [location.lon, location.lat],
-    //             label: {
-    //                 text: `${current.temp_c}°`,
-    //                 offset: [0, 25],
-    //                 relativeAnchor: [0.5, 0],
-    //                 color: "#fff",
-    //             },
-    //         });
-    //     });
-    //
-    //     // Удаляем карту при размонтировании компонента
-    //     return () => map && map.destroy();
-    // }, [location]);
+    const {location, current} = useCustomSelector(state => state.currentWeatherSliceReducer.weather)
+
+    useEffect(() => {
+        let map: any;
+        load().then((mapglAPI) => {
+            map = new mapglAPI.Map('map-container', {
+                center: [location.lon, location.lat],
+                zoom: 11,
+                key: `${mapKey}`,
+                style: 'efac10b3-c66c-40de-8165-decca561250c'
+            });
+
+            const marker = new mapglAPI.Marker(map, {
+                coordinates: [location.lon, location.lat],
+                label: {
+                    text: `${Math.round(current.temp_c)}°`,
+                    offset: [0, 25],
+                    relativeAnchor: [0.5, 0],
+                    color: "#fff",
+                },
+            });
+        });
+
+        // Удаляем карту при размонтировании компонента
+        return () => map && map.destroy();
+    }, [location]);
 
     return (
         <div className={cl.wrapper}>
             <div className={cl.map}>
-                {/*<Map />*/}
+                <Map />
             </div>
         </div>
     );
